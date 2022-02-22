577db69754d2d4d05c73320fcea97771fecbc586
Nicholas Hernandez <83328450+nhernandez1957@users.noreply.github.com>
2022-02-22 13:02:56 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def index():
+    if 'visits' not in session:
+        session ['visits'] = 1 
+    else:
+        session ['visits'] += 1
+    return render_template("index.html", visits = session['visits'])
+
+@app.route('/destroy_session')
+def destroy():
+    session.pop('visits')
+    return render_template("index.html")
+
+if __name__=="__main__":  
+    app.run(debug=True, port = 5001)  
